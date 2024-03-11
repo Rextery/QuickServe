8bdd9d98d556bd3e874ed6ded1d92529a3c7ce0a
JChaverra21 <jose_chaverra82182@elpoli.edu.co>
2024-03-10 20:57:59 -0500

    Se agrega el nuevo componente ShoppingCart y ajustes de estilos

--- client/Front-QuickServe/src/components/Home.jsx
+++ client/Front-QuickServe/src/components/Home.jsx
@@ -1,18 +1,28 @@
 import Navbar from "./Navbar";
 import Card from "./Card";
 import Footer from "./Footer";
+import ShoppingCart from "./ShoppingCart";
 
 const Home = () => {
   return (
     <main>
       <Navbar />
-      {/* <div className="container mx-auto my-8">
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          <Card />
-        </div>
-      </div> */}
-      <div className="container my-8 grid">
+      {/* <div className="container m-8 grid">
         <Card />
+      </div> */}
+      <div className="container m-8 mt-16 grid">
+        <div className="flex flex-row gap-8">
+          <div className="flex md:basis-1/2 lg:basis-1/2">
+            <Card />
+          </div>
+          <div className="md:basis-1/2 lg:basis-1/2">
+            <div className="md:max-w-[400px] lg:max-w-[750px]">
+              <div className="hidden md:flex">
+                <ShoppingCart />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </main>
